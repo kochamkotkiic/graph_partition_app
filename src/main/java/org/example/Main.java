a394aa9a9f7e415dbd46ad8c38300e0dc78c9bcd
kochamkotkiic <01197869@pw.edu.pl>
2025-05-16 21:56:09 +0200

    adjusting the ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame();
+            // Tutaj dodaj kod zmieniający ikonę
+            ImageIcon icon = new ImageIcon("src/images/kitty.jpg");
+            frame.setIconImage(icon.getImage());
+            // Koniec kodu ikony
             frame.setVisible(true);
         });
     }
